16e679a81867bbaca73ddedf96011053b47ebd16
Ienca1892 <124431624+Ienca1892@users.noreply.github.com>
2023-07-13 19:16:40 +0200

    eeeee

---
+++ Il Libro su Javascript/ilmioJS.js
@@ -0,0 +1,45 @@
+// "Data di oggi"
+
+// var data = new Date ();
+// alert(data);
+
+
+/* let x = 2;
+let y = 2;
+console.assert(x + y == 5, "Risultato sbagliato");
+let A = 5;
+console.log(A > 3, "Purtroppo A e' maggiore di 3,quindi abbiamo un problema..")
+ */
+
+
+/* let x = 2;
+let y = 3;
+console.assert(x ** y == 50, "Risultato sbagliato"); */
+
+/* document.getElementById("abc").addEventListener("click", function () {
+            alert("Body");
+        })
+
+        document.getElementById("provaclick").addEventListener("click", function () {
+            alert("Div");
+        }, true)
+
+        document.getElementById("pulsante").addEventListener("click", function () {
+            alert("Button");
+        }) */
+
+        /* let a = 3;
+        function aggiungisei(x) {
+            let ret = x + 6
+            return ret
+        }
+        let b = aggiungisei(a)
+        console.log(b); */
+
+        function esecuzione() {
+            aggiunta("Devo studiare!");
+        }
+        function aggiunta(altro) {
+            alert("Questo e' l'argomento ricevuto da esecuzione(): " + altro ";
+        }
+        esecuzione();
